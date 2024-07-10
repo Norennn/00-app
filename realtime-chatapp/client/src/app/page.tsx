9adca8a0eae3c7232f0181eb93adb2b9ad3a298e
Noren <hinorennn0221@gmail.com>
2024-07-10 18:21:53 +0900

    dev chat app

--- realtime-chatapp/client/src/app/page.tsx
+++ realtime-chatapp/client/src/app/page.tsx
@@ -7,7 +7,7 @@ import React, { useState, useEffect } from "react";
 
 // 環境変数を使用して接続先を設定
 const socket = io(
-  process.env.NEXT_PUBLIC_SOCKET_URL || "https://zero0-app.onrender.com:8000"
+  process.env.NEXT_PUBLIC_SOCKET_URL || "https://zero0-app.onrender.com"
 );
 
 export default function Home() {
